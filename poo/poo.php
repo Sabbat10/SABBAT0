599487a8d50eab9d7b4b61bc566bba7fe2b785e4
sabbat10 <sabbatlumpatshia99@gmail.com>
2022-07-20 11:20:04 +0100

    Cours sur le POO

--- poo/poo.php
+++ poo/poo.php
@@ -1,57 +1,125 @@
-
 <!DOCTYPE html>
 <html>
+
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<title></title>
 </head>
+
 <body>
 
-<?php 
+	<?php
 
-	class Voiture{	
+	class Voiture
+	{
 
-		private $_nom 		= 'Toyota';
-		private $_couleur 	= 'Noire';
-		private $_modele      = 'S4v';
+		private $_nom;
+		private $_couleur;
+		private $_modele;
+
+		//Les contruceteurs
+
+		public function __construct($nom = 'Tesla', $couleur = 'noire', $modele = 'S-16')
+		{
+			$this->setNom($nom);
+			$this->setCouleur($couleur);
+			$this->setModele($modele);
+		}
 
 		//getter
-		public function getNom() {
+		public function getNom()
+		{
 			return $this->_nom;
 		}
-		public function getCouleur() {
+		public function getCouleur()
+		{
 			return $this->_couleur;
 		}
-		public function getModele() {
+		public function getModele()
+		{
 			return $this->_modele;
 		}
 
 		//Setter
 
-		public function setNom($newNom) {
+		public function setNom($newNom)
+		{
 			$this->_nom = $newNom;
 		}
-		public function setCouleur($newCouleur) {
+		public function setCouleur($newCouleur)
+		{
 			$this->_couleur = $newCouleur;
 		}
+		public function setModele($newModele)
+		{
+			$this->_modele = $newModele;
+		}
+
+		function souhait($annees = '2 ans', $mois = 'Juin')
+		{
+	?>
+			<p>Je souhaite acheter un <strong><i class="ss"><?php echo $this->getNom(); ?></i></strong> de la couleur <strong><i class="ss"><?php echo $this->getCouleur(); ?></i></strong> modèle <strong><i class="ss"><?php echo $this->getModele(); ?></i><?php echo ' dans ' . $annees . ' au mois de ' . $mois ?></strong></p>
+	<?php
+
+		}
 	}
-	$sabbat = new Voiture();
+	$sabbat = new Voiture('RAV-4', 'Rouge', 'V8');
+	// $sabbat->setNom('RAV-4');
+	// $sabbat->setCouleur('Blue-Ciel');
+	$sabbat->souhait('5 ans', 'Decembre');
+
 	$alliance = new Voiture();
+	$alliance->souhait();
 
-	$sabbat->setNom('RAV4');
-	$sabbat->setCouleur('Blue-Ciel');
+	$daniela = new Voiture('Trasco', 'Blanche', 'FT',);
+	$daniela->souhait('5 mois', 'Novembre');
+
+
+
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	class Humain
+	{
+
+		private $vivant = true;
+
+		public function riche()
+		{
+			echo 'Je suis riche !';
+		}
+
+		public function sante()
+		{
+			echo ' Je suis en bonne sante !<br>';
+		}
+	}
+
+	class Mark extends Humain
+	{
+	}
+
+	class Patrice extends Humain
+	{
+
+		private $vivant = false;
+
+		public function sante()
+		{
+			echo ' Je ne suis pas en bonne sante !';
+		}
+	}
+
+	$mark = new Mark();
+	$mark->riche() . ' et ' . $mark->sante();
+
+	$patrice = new Patrice();
+	$patrice->riche() . '' . $patrice->sante();
+
+
 
 	?>
-		<p>Je souhaite acheter un <strong><i class="ss"><?php echo $sabbat->getNom(); ?></i></strong> de la couleur <strong ><i class="ss"><?php echo $sabbat->getCouleur(); ?></i></strong> modèle <strong><i  class="ss"><?php echo $sabbat->getModele(); ?></i></strong></p>
-	<?php 
-
-	?>
-		<p>Je souhaite acheter un <strong><i><?php echo $alliance->getNom(); ?></i></strong> de la couleur <strong><i><?php echo $alliance->getCouleur(); ?></i></strong> modèle <strong><i><?php echo $alliance->getModele(); ?></i></strong></p>
-	<?php 
-
-?>
 </body>
-</html>
 
+</html>
